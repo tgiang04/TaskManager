780ac0b0d5c44ee35a7bd265f2cfcb4637cc6881
Giang <killkazaki2k4@gmail.com>
2026-02-03 00:44:57 +0700

    Initial commit: Upload NukeViet source code to TaskManager

--- src/data/config/robots.php
+++ src/data/config/robots.php
@@ -4,7 +4,7 @@
  * NukeViet Content Management System
  * @version 5.x
  * @author VINADES.,JSC <contact@vinades.vn>
- * @copyright (C) 2009-2025 VINADES.,JSC. All rights reserved
+ * @copyright (C) 2009-2026 VINADES.,JSC. All rights reserved
  * @license GNU/GPL version 2 or any later version
  * @see https://github.com/nukeviet The NukeViet CMS GitHub project
  */
@@ -14,4 +14,4 @@ if (!defined('NV_MAINFILE')) {
 }
 
 $cache = 'a:6:{s:6:"/data/";i:0;s:10:"/includes/";i:0;s:9:"/install/";i:0;s:9:"/modules/";i:0;s:11:"/robots.php";i:0;s:11:"/web.config";i:0;}';
-$cache_other = 'a:3:{s:18:"/vi/banners/click/";i:0;s:15:"/vi/statistics/";i:0;s:10:"/vi/users/";i:0;}';
+$cache_other = 'a:6:{s:18:"/vi/banners/click/";i:0;s:15:"/vi/statistics/";i:0;s:10:"/vi/users/";i:0;s:31:"/nukeviet/src/vi/banners/click/";i:0;s:28:"/nukeviet/src/vi/statistics/";i:0;s:23:"/nukeviet/src/vi/users/";i:0;}';
